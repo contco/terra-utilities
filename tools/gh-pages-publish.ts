93e1e90d42ad916b7507e198dfd11121dca8ae00
Fahad-Mahmood <fahad492@outlook.com>
2021-09-28 17:51:08 +0500

    gh-pages deploy script updated

--- tools/gh-pages-publish.ts
+++ tools/gh-pages-publish.ts
@@ -1,24 +1,13 @@
-const { cd, exec, echo, touch } = require("shelljs")
-const { readFileSync } = require("fs")
-const url = require("url")
+const ghpages = require('gh-pages');
 
-const repoUrl = "https://github.com/contco/terra-utilities.git";
-let pkg = JSON.parse(readFileSync("package.json") as any)
-
-
-let parsedUrl = url.parse(repoUrl)
-let repository = (parsedUrl.host || "") + (parsedUrl.path || "")
-let ghToken = process.env.GH_TOKEN
-
-echo("Deploying docs!!!")
-cd("docs")
-touch(".nojekyll")
-exec("git init")
-exec("git add .")
-exec('git config user.name "Fahad-Mahmood"')
-exec('git config user.email "fahad492@outlook.com"')
-exec('git commit -m "docs(docs): update gh-pages"')
-exec(
-  `git push --force --quiet "https://${ghToken}@${repository}" master:gh-pages`
-)
-echo("Docs deployed!!")
+ghpages.publish('docs', {
+  branch: "main",
+  repo: "https://github.com/contco/terra-utilities.git"
+}, (err: any) => {
+  if(err) {
+  console.log(err)
+  }
+  else {
+    console.log("Successfully Deployed");
+  }
+});
