07d2ae617f83e6e5f9e7f240fd7c5073f184430a
Fahad-Mahmood <fahad492@outlook.com>
2021-09-28 17:39:42 +0500

    block time ms conversion added

--- src/block/latestBlockTime.ts
+++ src/block/latestBlockTime.ts
@@ -5,7 +5,8 @@ const {LCD_URL} = constants;
 export const getLatestBlockTime = async (lcdUrl = LCD_URL) => {
     const response = await fetch( lcdUrl+ 'blocks/latest' );
     const block = await response.json();
-    const dateTime = new Date(block.block.header.time);
-    return dateTime.getTime();
+    const blockTimeInMs = new Date(block.block.header.time).getTime();
+    const blockTime = Math.round(blockTimeInMs / 1000);
+    return blockTime;
 };
   
