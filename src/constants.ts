d6439094f8ca4dac609582dd565a5cbedb6fa054
Fahad-Mahmood <fahad492@outlook.com>
2021-10-10 19:46:12 +0500

    tefi lcd url added

--- src/constants.ts
+++ src/constants.ts
@@ -1,5 +1,5 @@
 export const UUSD_DENOM =  'uusd';
 export const LUNA_DENOM = 'uluna';
 export const MICRO = 1000000;
-export const LCD_URL = "http://104.154.220.59/";
+export const LCD_URL = "https://berkerden.com/";
 export const FCD_URL =  "https://fcd.terra.dev/";
