abed18738c803085f9e61ee65ab5cce7c553a656
Fahad-Mahmood <fahad492@outlook.com>
2021-10-14 02:45:08 +0500

    lcd url updated, tefi_api url added

--- src/constants.ts
+++ src/constants.ts
@@ -1,5 +1,6 @@
 export const UUSD_DENOM =  'uusd';
 export const LUNA_DENOM = 'uluna';
 export const MICRO = 1000000;
-export const LCD_URL = "https://berkerden.com/";
-export const FCD_URL =  "https://fcd.terra.dev/";
+export const LCD_URL = "https://lcd.contco.dev/";
+export const TEFI_API = "https://api.contco.dev/";
+export const FCD_URL =  TEFI_API + "https://fcd.terra.dev/";
